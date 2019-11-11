5adbedfb8205bdbf9e7a4430ca00bbac40944ca7
Douglas Gardiner <doug.gardiner@gmail.com>
2019-11-11 12:28:21 -0600

    Changed the setup script around because order matters when dropping tables that have foreign keys and because I forgot to add teams.  At the moment there is no heirarchy for adding members to a team.  I may add that later.

--- setup.sql
+++ setup.sql
@@ -1,12 +1,13 @@
 create database whiteboard;
 use whiteboard;
-drop table if exists `users`;
-drop table if exists `projects`;
-drop table if exists `milestones`;
-drop table if exists `sprints`;
-drop table if exists `project_users`;
-drop table if exists `tasks`;
 drop table if exists `comments`;
+drop table if exists `tasks`;
+drop table if exists `project_users`;
+drop table if exists `milestones`;
+drop table if exists `teams`;
+drop table if exists `sprints`;
+drop table if exists `projects`;
+drop table if exists `users`;
 	
 create table users (
 	id int auto_increment primary key,
@@ -25,6 +26,15 @@ create table projects (
 	deleted tinyint(1) not null default 0
 );
 
+create table teams (
+	id int auto_increment primary key,
+	project int not null,
+	foreign key fk_teams_project(project) references projects(id),
+	user int not null,
+	foreign key fk_teams_user(user) references users(id),
+	deleted tinyint(1) not null default 0
+);
+
 create table milestones (
 	id int auto_increment primary key,
 	name varchar(40) not null,
@@ -36,6 +46,8 @@ create table milestones (
 create table sprints (
 	id int auto_increment primary key,
 	project int not null,
+	start_date date not null,
+	end_date date not null,
 	foreign key fk_sprints_project(project) references projects(id),
 	deleted tinyint(1) not null default 0
 );
