c44aba18c1c70a85cf5d86fe8334d5dcf844f211
Douglas Gardiner <doug.gardiner@gmail.com>
2020-06-05 21:07:10 -0500

    fixed minor issue with setup

--- whiteboarduser_setup.sql
+++ whiteboarduser_setup.sql
@@ -5,4 +5,3 @@ create user 'whiteboarduser'@'localhost' identified by '1990BMW325!';
 grant select on *.* to 'whiteboarduser'@'localhost';
 grant select, insert, update, delete on whiteboard.* to 'whiteboarduser'@'localhost';
 flush privileges;
-create user 'root'@'10.0.0.%' identified by '91harley';
