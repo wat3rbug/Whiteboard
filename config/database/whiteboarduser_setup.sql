e74fa0ce42df002f7ad707f523fe637d75a392ad
Douglas Gardiner <doug.gardiner@gmail.com>
2025-04-23 20:22:55 -0500

    update the user stuff

--- config/database/whiteboarduser_setup.sql
+++ config/database/whiteboarduser_setup.sql
@@ -1,7 +1,7 @@
-create user 'whiteboarduser'@'10.0.0.%' identified by '1990BMW325!';
+create user 'whiteboarduser'@'10.0.0.%' identified by 'password';
 grant select on *.* to 'whiteboarduser'@'10.0.0.%';
 grant select, insert, update, delete on whiteboard.* to 'whiteboarduser'@'10.0.0.%';
-create user 'whiteboarduser'@'localhost' identified by '1990BMW325!';
+create user 'whiteboarduser'@'localhost' identified by 'password';
 grant select on *.* to 'whiteboarduser'@'localhost';
 grant select, insert, update, delete on whiteboard.* to 'whiteboarduser'@'localhost';
 flush privileges;
